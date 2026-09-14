8708ad6ab2c11e8033cd132e3118c9d4674901b7
Lawrence <lawuche29@gmail.com>
2026-09-14 02:05:21 +0100

    chore:set up

---
+++ hello_world/target/debug/deps/hello_world-2e935ef1eaba6a49.d
@@ -0,0 +1,5 @@
+/home/lawuche/rust_tuts/hello_world/target/debug/deps/hello_world-2e935ef1eaba6a49.d: src/main.rs
+
+/home/lawuche/rust_tuts/hello_world/target/debug/deps/hello_world-2e935ef1eaba6a49: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/lawuche/rust_tuts/hello_world/target/debug/hello_world: /home/lawuche/rust_tuts/hello_world/src/main.rs
